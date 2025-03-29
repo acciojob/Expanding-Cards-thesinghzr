620b7080b28ce7801206c658471662a7a1ba0153
Suraj Singh <yousurajsingh@gmail.com>
2025-03-29 23:18:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+    const panels = document.querySelectorAll('.panel');
+
+    // Set the first panel as active by default
+    panels[0].classList.add('active');
+
+    panels.forEach(panel => {
+        panel.addEventListener('click', () => {
+            removeActiveClasses();
+            panel.classList.add('active');
+        });
+    });
+
+    function removeActiveClasses() {
+        panels.forEach(panel => {
+            panel.classList.remove('active');
+        });
+    }
+});
